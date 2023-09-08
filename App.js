e7e2300daa1de5d40d16f235e591f55e15d1c00d
agnieszkaholewik <agnieszka.holewik01@gmail.com>
2023-09-08 12:49:55 +0200

    amendments

--- App.js
+++ App.js
@@ -1,28 +1,32 @@
 import CategoriesScreen from './screens/CategoriesScreen';
 import LaunchScreen from './screens/LaunchScreen';
 import { StatusBar } from 'expo-status-bar';
-import { NavigationContainer } from '@react-navigation/native'
-import { createStackNavigator } from '@react-navigation/stack'
+import { NavigationContainer} from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import SubCategoriesScreen from './screens/subCategoriesScreen';
-
-
 import 'react-native-gesture-handler'
 
-const Stack = createStackNavigator();
+
+const Stack = createNativeStackNavigator();
+
+
+
+
 
 
 export default function App() {
   return (
     <>
       <StatusBar style='auto' />
-      <NavigationContainer>
-        <Stack.Navigator screenOptions={{headerStyle: { backgroundColor: '#b0c4de'},headerTintColor: 'black', headerBackTitle:'Back'}}>
-          <Stack.Screen name="LaunchScreen" component={LaunchScreen} options={{ headerShown: false }} />
-          <Stack.Screen name="CategoriesScreen" component={CategoriesScreen} options={{ title: 'Categories'}} />
-          <Stack.Screen name="SubCategoriesScreen" component={SubCategoriesScreen}/>
+      <NavigationContainer >
+        <Stack.Navigator screenOptions={{headerStyle: { backgroundColor: '#b0c4de'}, headerTintColor: 'black', headerBackTitle:'Back'}}>
+          <Stack.Screen name="LaunchScreen" component={LaunchScreen} options={{ headerShown: false}} />
+          <Stack.Screen name="CategoriesScreen" component={CategoriesScreen} options={{ title: 'Categories' }}  />
+          <Stack.Screen name="SubCategoriesScreen" component={SubCategoriesScreen} />
         </Stack.Navigator>
       </NavigationContainer>
     </>
+    
 
 
   );
